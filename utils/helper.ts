e7c57844894354bfa50f4eaf938895cc82c8097c
多厘 <zhaody901@126.com>
2022-03-09 16:36:52 +0800

    feat: 验证 simple pwd 规则 close #3

--- utils/helper.ts
+++ utils/helper.ts
@@ -5,10 +5,7 @@
 |
 */
 
-import { MD5 } from "crypto-js";
-import { pyAppMode, pyStorageKey } from "@/framework/utils/conf";
-import { camelCase, each, forEach, get, indexOf, isNull, isString, random, round, upperFirst } from "lodash-es";
-import { ElMessage } from "element-plus/es";
+import { camelCase, get, indexOf, isNull, random, round, upperFirst } from "lodash-es";
 
 
 /**
@@ -21,98 +18,6 @@ export function uniqueId(prefix?: string) {
     return _pre + Math.floor(Math.random() * (new Date()).getTime());
 }
 
-/**
- * 设置获取Store 的内容
- * @param {string|array} key
- * @param val
- * @returns
- */
-function _localStore(key: any, val: any) {
-    /**
-     * localStorage内存溢出时，则清空后继续保存
-     * @param {string} key 缓存key
-     * @param {string} data JSON.stringify后的数据
-     */
-    let _localStorageOverflow = (key: any, data: any) => {
-        try {
-            localStorage.setItem(key, data);
-        } catch (e: any) { // 当缓存溢出，则清空后继续保存
-            if (e.code === 'QUOTA_EXCEEDED_ERR_CODE') {
-                localStorage.removeItem(key);
-                localStorage.setItem(key, data);
-            } else {
-                console.error(e);
-            }
-        }
-    };
-
-    // 本地数据存储封装，没有过期时间限制，仅限于该页面的协议
-    if (val === null) {
-        if (typeof key === 'object') {
-            each(key, function (ele, idx) {
-                _localStorageOverflow(idx, ele); // 存储数据
-            });
-            return;
-        } else {
-            localStorage.removeItem(key);// 移除数据
-            return;
-        }
-    }
-    if (typeof val === 'undefined') {
-        let data = localStorage.getItem(key);
-        if (data) {
-            try {
-                data = JSON.parse(data);
-                return data; // 获取数据
-            } catch (err) {
-                return data; // 获取数据
-            }
-        }
-        return data; // 获取数据
-    }
-    if (typeof val === 'object') {
-        _localStorageOverflow(key, JSON.stringify(val));
-    } else {
-        _localStorageOverflow(key, val);
-    }
-}
-
-
-/**
- * 实现sessionStorage缓存的 存, 取, 删操作
- * @param key 对象, 批量设置
- * @param val 有值:设置; 无值: 获取; null, 删除;
- */
-function _sessionStore(key: any, val: any) {  // 本地数据存储封装，随页面回话结束而结束，仅限于该页面的协议
-    if (val === null) {
-        if (typeof key === 'object') {
-            forEach(key, function (ele, idx) {
-                sessionStorage.setItem(idx, ele);
-            });
-            return;
-        } else {
-            sessionStorage.removeItem(key);
-            return;
-        }
-    }
-    if (typeof val === 'undefined') {
-        let data = sessionStorage.getItem(key);
-        if (data) {
-            try {
-                data = JSON.parse(data);
-                return data; // 获取数据
-            } catch (err) {
-                return sessionStorage.getItem(key); // 获取数据
-            }
-        }
-        return data; // 获取数据
-    }
-    if (typeof val === 'object') {
-        sessionStorage.setItem(key, JSON.stringify(val));
-    } else {
-        sessionStorage.setItem(key, val);
-    }
-}
 
 /**
  * 返回根据大小所匹配的 class name
@@ -168,20 +73,6 @@ export const base64Decode = (data: string) => {
     return window.atob(data);
 }
 
-
-/**
- * 返回设备ID, 如果本地存在则取本地
- */
-export const deviceId = (): string => {
-    const val = localStore(pyStorageKey.deviceId)
-    if (val) {
-        return val;
-    } else {
-        let id = 'h-' + MD5(uniqueId('poppy')) + '-5'
-        localStore(pyStorageKey.deviceId, id);
-        return id;
-    }
-}
 
 
 /*
@@ -358,7 +249,7 @@ export const isUsername = (val: string, type = 'normal') => {
  * @param val
  */
 export const isSimplePwd = (val: string) => {
-    return regexTest(val, /([0-9a-zA-Z_*.\[\]\-!@#$%^&()~]+){6,16}/i)
+    return regexTest(val, /^[0-9a-zA-Z_*.\[\]\-!@#$%^&()~]{6,16}$/i)
 }
 
 /**
@@ -578,7 +469,7 @@ export const isWechat = () => {
  */
 export const isMobile = function (str: string) {
     let phone_number = str.replace(/\(|\)|\s+|/g, '');
-    return phone_number.length > 10 && phone_number.match(/^(\d{1,5}\-)?1[3|4|5|6|8|7|9][0-9]\d{4,8}$/);
+    return phone_number.length > 10 && phone_number.match(/^(\d{1,5}-)?1[3|4|5|6|8|7|9][0-9]\d{4,8}$/);
 };
 
 
@@ -591,69 +482,8 @@ export const upperCamelCase = (str: string) => {
 }
 
 
-/**
- * 实现localStorage缓存的 存, 取, 删操作
- * @param key 对象, 批量设置
- * @param val 有值:设置; 无值: 获取; null, 删除;
- */
-export const localStore = (key: any, val?: any) => {
-    return _localStore(hashKey(key), val);
-};
-
-/**
- * 实现sessionStorage缓存的 存, 取, 删操作
- * @param key 对象, 批量设置
- * @param val 有值:设置; 无值: 获取; null, 删除;
- */
-export const sessionStore = (key: any, val?: any) => {
-    return _sessionStore(hashKey(key), val);
-};
 
 
-/**
- * 通过域名 + 版本号摒弃缓存
- * 这个地方和清空缓存有关系, 注意使用, 这里必须要有 `:`
- * @returns {*}
- */
-export const hashKey = (key: string) => {
-    let genKey = `${pyAppMode}:${key}`;
-    if (genKey.indexOf(':') < 0) {
-        console.error('生成的KEY不包含 :, 会导致清理缓存异常');
-    }
-    return genKey;
-};
-
-
-/**
- * 封装 ele 的状态显示
- * @param {string|object} resp
- * @param {boolean|string|integer} warning
- */
-export const toast = (resp: any, warning: any = true) => {
-    let type = warning;
-    if (isInteger(warning)) {
-        type = Boolean(warning);
-    }
-    if (isString(resp)) {
-        if (type === true) {
-            ElMessage.success(resp);
-        } else if (type === false) {
-            ElMessage.warning(resp);
-        } else {
-            ElMessage.error(resp);
-        }
-
-    } else {
-        const { message, status } = resp;
-        if (status === 0) {                         // 成功
-            ElMessage.success(message)
-        } else if (status > 0 && status <= 1000) {  // http error
-            ElMessage.error(message)
-        } else {                                    // project error
-            ElMessage.warning(message);
-        }
-    }
-}
 
 /**
  * 组合请求Url
