ac6099c55fabff72b53168a647d07ca571e43eb6
多厘 <zhaody901@126.com>
2022-06-24 22:02:46 +0800

    feat: 支持 FormTable 的数据管理

--- src/components/form/FieldTable.vue
+++ src/components/form/FieldTable.vue
@@ -7,15 +7,13 @@
                 <template #default="{row}">
                     <ColumnText v-if="get(col, 'type') === 'text'" :value="get(row, col.field)"
                         :ellipsis="get(col, 'ellipsis', false)" :copyable="get(col, 'copyable', false)"
-                        :editable="get(col, 'editable', '')" :attr="get(col, 'edit-attr', {})" :field="get(col, 'field')" :pk-id="row[props.pk]"
+                        :editable="get(col, 'editable', '')" :attr="get(col, 'edit-attr', {})" :field="get(col, 'field')" :pk-id="row['_idx']"
                         @update="onUpdateCell"/>
-                    <ColumnHidden v-else-if="get(col, 'type') === 'hidden'" :value="get(row, col.field)"
-                        :pk-id="get(row, [props.pk])" :attr="get(col, 'edit-attr', {})" :field="get(col, 'field')"/>
                     <ColumnOnOff v-else-if="get(col, 'type') === 'on-off'" :value="get(row, col.field)"
-                        :editable="get(col, 'editable', '')" :attr="get(col, 'edit-attr', {})" :field="get(col, 'field')" :pk-id="row[props.pk]"
+                        :editable="get(col, 'editable', '')" :attr="get(col, 'edit-attr', {})" :field="get(col, 'field')" :pk-id="row['_idx']"
                         @update="onUpdateCell"/>
                     <ColumnSelect v-else-if="get(col, 'type') === 'select'" :value="get(row, col.field)"
-                        :editable="get(col, 'editable', '')" :attr="get(col, 'edit-attr', {})" :field="get(col, 'field')" :pk-id="row[props.pk]"
+                        :editable="get(col, 'editable', '')" :attr="get(col, 'edit-attr', {})" :field="get(col, 'field')" :pk-id="row['_idx']"
                         @update="onUpdateCell"/>
                     <ColumnLink v-else-if="get(col, 'type') === 'link'"
                         :ellipsis="get(col, 'ellipsis', false)" :value="get(row, col.field)"/>
@@ -25,26 +23,37 @@
                         :value="get(row, col.field)"/>
                     <ColumnHtml v-else-if="get(col, 'type') === 'html'"
                         :value="get(row, col.field)"/>
-                    <ColumnActions v-else-if="get(col, 'type') === 'actions'"
-                        :value="get(row, col.field)"/>
+                    <ColumnTableAction v-else-if="get(col, 'type') === 'table-action'"
+                        :is-top="get(row, '_idx') === refTopIdx"
+                        :is-last="get(row, '_idx') === refLastIdx"
+                        @modify="onTableAction"
+                        :pk="get(row, '_idx')" :editable="get(col, 'editable', [])"/>
                     <template v-else>{{ get(row, col.field) }}</template>
                 </template>
             </ElTableColumn>
         </template>
+        <template #empty>
+            <ElButton class="el-button-has-mu" @click="onAdd">
+                <XIcon type="mu:new_label"/>
+                添加一行
+            </ElButton>
+        </template>
     </ElTable>
 </template>
 <script lang="ts" setup>
-import { clone, find, get, isObjectLike, set } from 'lodash-es';
-import { onMounted, ref, watch } from "vue";
+import { clone, each, find, first, get, isObjectLike, last, map, remove, set } from 'lodash-es';
+import { computed, onMounted, ref } from "vue";
 import ColumnText from "@/components/grid/ColumnText.vue";
-import ColumnHidden from "@/components/grid/ColumnHidden.vue";
 import ColumnOnOff from "@/components/grid/ColumnOnOff.vue";
 import ColumnSelect from "@/components/grid/ColumnSelect.vue";
 import ColumnLink from "@/components/grid/ColumnLink.vue";
 import ColumnImage from "@/components/grid/ColumnImage.vue";
 import ColumnDownload from "@/components/grid/ColumnDownload.vue";
 import ColumnHtml from "@/components/grid/ColumnHtml.vue";
-import ColumnActions from "@/components/grid/ColumnActions.vue";
+import ColumnTableAction from "@/components/grid/ColumnTableAction.vue";
+import XIcon from "@/components/element/XIcon.vue";
+import dayjs from "dayjs";
+import { toast } from "@/utils/util";
 
 
 const props = defineProps({
@@ -64,6 +73,12 @@ const emit = defineEmits([
 const refOriValue = ref<any[]>([]);
 
 
+const onAdd = () => {
+    onTableAction({
+        type: 'add',
+    })
+}
+
 /**
  * 进行修改
  * @param obj
@@ -76,7 +91,7 @@ const onUpdateCell = (obj: object) => {
     let field = get(obj, 'field');
     let value = get(obj, 'value');
     // 值变动请求服务器
-    let row = find(refOriValue.value, { [props.pk]: pk }) as object;
+    let row = find(refOriValue.value, { '_idx': pk }) as object;
 
     // 当前变动的对象是 {value} | value
     let cpOriVal = get(row, [field]);
@@ -92,9 +107,111 @@ const onUpdateCell = (obj: object) => {
     set(row, [field], cpEditVal);
 }
 
+const createEmptyValue = () => {
+    let row = {
+        _idx: 'new:' + dayjs().millisecond()
+    };
+    each(get(props.attr, 'cols'), function (item) {
+        if (get(item, 'type') !== 'table-action') {
+            if (get(item, 'editable')) {
+                set(row, [item.field], {
+                    value: '',
+                    disabled: false,
+                });
+            } else {
+                set(row, [item.field], '');
+            }
+        }
+    })
+    return row;
+}
+
+const refTopIdx = computed(() => {
+    let topVal = first(refOriValue.value);
+    return get(topVal, '_idx');
+})
+const refLastIdx = computed(() => {
+    let topVal = last(refOriValue.value);
+    return get(topVal, '_idx');
+})
+
+/**
+ * 进行修改
+ * @param obj
+ */
+const onTableAction = (obj: object) => {
+    let type = get(obj, 'type');
+    let idx = get(obj, 'idx');
+
+    switch (type) {
+        case 'delete':
+            remove(refOriValue.value, {
+                '_idx': idx
+            })
+            break;
+        case 'add':
+            console.log(idx);
+            if (!idx) {
+                // 空数据添加
+                refOriValue.value.push(createEmptyValue());
+                return;
+            }
+            let idxAdd = 1;
+            each(refOriValue.value, function (item, key) {
+                if (get(item, '_idx') === idx) {
+                    idxAdd = key + 1
+                }
+            })
+            refOriValue.value.splice(idxAdd, 0, createEmptyValue());
+            break;
+        case 'down':
+            let idxDown = 0;
+            each(refOriValue.value, function (item, key) {
+                if (get(item, '_idx') === idx) {
+                    idxDown = key
+                }
+            })
+            if (idxDown + 1 === refOriValue.value.length) {
+                toast('已经是最后一个');
+                return;
+            }
+            [refOriValue.value[idxDown], refOriValue.value[idxDown + 1]] = [refOriValue.value[idxDown + 1], refOriValue.value[idxDown]]
+            break;
+        case 'up':
+            let idxUp = 0;
+            each(refOriValue.value, function (item, key) {
+                if (get(item, '_idx') === idx) {
+                    idxUp = key
+                }
+            })
+            if (idxUp === 0) {
+                toast('已经是第一个');
+                return;
+            }
+            [refOriValue.value[idxUp - 1], refOriValue.value[idxUp]] = [refOriValue.value[idxUp], refOriValue.value[idxUp - 1]]
+            break;
+        case 'copy':
+            let idxCopy = 0;
+            let valCopy = {}
+            each(refOriValue.value, function (item, key) {
+                if (get(item, '_idx') === idx) {
+                    idxCopy = key;
+                    valCopy = item;
+                }
+            })
+            set(valCopy, '_idx', dayjs().millisecond())
+            refOriValue.value.splice(idxCopy, 0, valCopy);
+            break;
+    }
+}
+
 onMounted(() => {
     // 需要对数据进行处理加入默认PK
-    refOriValue.value = props.modelValue;
+    refOriValue.value = map(props.modelValue, (val: any, index) => {
+        let idx = `old:${index}`;
+        set(val, '_idx', idx);
+        return val;
+    })
 })
 
 </script>
---
+++ src/components/grid/ColumnTableAction.vue
@@ -0,0 +1,81 @@
+<template>
+    <ElButton circle class="el-button-has-mu" size="small" @click="onAdd">
+        <XIcon type="mu:new_label"/>
+    </ElButton>
+    <ElButton circle class="el-button-has-mu" size="small" @click="onDown" :disabled="isLast">
+        <XIcon type="mu:arrow_downward"/>
+    </ElButton>
+    <ElButton circle class="el-button-has-mu" size="small" @click="onUp" :disabled="isTop">
+        <XIcon type="mu:arrow_upward"/>
+    </ElButton>
+    <ElButton circle class="el-button-has-mu" size="small" @click="onCopy">
+        <XIcon type="mu:content_copy"/>
+    </ElButton>
+    <ElButton circle class="el-button-has-mu" type="danger" size="small" @click="onDelete">
+        <XIcon type="mu:backspace"/>
+    </ElButton>
+</template>
+<script lang="ts" setup>
+import XIcon from "@/components/element/XIcon.vue";
+
+const props = defineProps({
+    editable: {
+        type: Array,
+        default: () => {
+            return []
+        }
+    },
+    isTop: {
+        type: Boolean,
+        default: false
+    },
+    isLast: {
+        type: Boolean,
+        default: false
+    },
+    pk: {
+        type: String,
+        required: true,
+        default: ''
+    }
+})
+
+const emits = defineEmits([
+    'modify',
+])
+
+const onAdd = () => {
+    emits('modify', {
+        idx: props.pk,
+        type: 'add',
+    })
+}
+const onDelete = () => {
+    emits('modify', {
+        idx: props.pk,
+        type: 'delete',
+    })
+}
+const onCopy = () => {
+    emits('modify', {
+        idx: props.pk,
+        type: 'copy',
+    })
+}
+const onDown = () => {
+    emits('modify', {
+        idx: props.pk,
+        type: 'down',
+    })
+}
+const onUp = () => {
+    emits('modify', {
+        idx: props.pk,
+        type: 'up',
+    })
+}
+
+</script>
+<style lang="less" scoped>
+
+</style>
